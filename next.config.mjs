d9c186466b1bad240f96ec936ec9aae6c4a2eeec
KYRGYZCODE <kyrgyzcode@gmail.com>
2025-08-09 16:13:15 +0500

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    domains: ['outdoorvitals.com', 'magic.qstorage.bilimmotion.com', 'back.ajnails.soulist.life'],
+  },
+}
+
+export default nextConfig
